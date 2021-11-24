af8ba7a284cc9a6d7afcea1ea9ce6b6d3f73dac8
Nainik <nainik.m@crestinfosystems.net>
2021-11-24 15:10:33 +0530

    Table and pagination completed

--- src/views/SurveyList/DataList.tsx
+++ src/views/SurveyList/DataList.tsx
@@ -1,6 +1,6 @@
 import { Table } from 'antd'
 import { ColumnsType } from 'antd/es/table'
-import React, { useEffect, useState } from 'react'
+import React, { ReactNode, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { ParsedFilter } from '../../helpers/common-types'
 import { getAllCovidSurvey } from '../../redux/actions/covidSurvey'
@@ -10,10 +10,9 @@ import { AppDispatch, RootState } from '../../redux/reducers/rootReducers'
 const DataList: React.FC = () => {
   const dispatch: AppDispatch = useDispatch()
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [parsedFilter, setParsedFilter] = useState<ParsedFilter>({
-    page: 0,
-    perPage: 20,
+    page: 1,
+    perPage: 10,
     search: '',
   })
   const [dataListLoading, setDataListLoading] = useState<boolean>(false)
@@ -21,14 +20,14 @@ const DataList: React.FC = () => {
   const dataList: covidObj[] = useSelector(
     (state: RootState) => state.covidSurveyReducer.covidSurveyList
   )
-  const totalPages = useSelector(
-    (state: RootState) => state.covidSurveyReducer.totalPages
-  )
-  const count = useSelector(
+
+  //   const totalPages = useSelector(
+  //     (state: RootState) => state.covidSurveyReducer.totalPages
+  //   )
+  const dataCount = useSelector(
     (state: RootState) => state.covidSurveyReducer.count
   )
 
-  console.log(dataList, totalPages, count, dataListLoading)
   useEffect(() => {
     setDataListLoading(true)
     dispatch(getAllCovidSurvey(parsedFilter)).then(() =>
@@ -42,10 +41,39 @@ const DataList: React.FC = () => {
       dataIndex: 'first_name',
       key: 'first_name',
     },
+    {
+      title: 'Last Name',
+      dataIndex: 'last_name',
+      key: 'last_name',
+    },
   ]
+
   return (
     <div>
-      <Table<covidObj> dataSource={dataList} columns={columns} rowKey="id" />
+      <Table<covidObj>
+        dataSource={dataList}
+        columns={columns}
+        rowKey="id"
+        loading={dataListLoading}
+        size="middle"
+        pagination={{
+          position: ['bottomCenter'],
+          total: dataCount,
+          showTotal: (total: number, range: [number, number]): ReactNode =>
+            `${range[0]}-${range[1]} of ${total} items`,
+          current: parsedFilter.page,
+          onChange: (page: number) => {
+            setParsedFilter({ ...parsedFilter, page })
+          },
+          pageSize: parsedFilter.perPage,
+          pageSizeOptions: ['10', '15', '20', '50'],
+          responsive: true,
+          showSizeChanger: true,
+          onShowSizeChange: (current: number, size: number) => {
+            setParsedFilter({ ...parsedFilter, perPage: size })
+          },
+        }}
+      />
     </div>
   )
 }
